402e0ee99592ee8f885284b30bab467b605c659c
MR-Khan0099 <mohammedrizwan0909@gmail.com>
2025-08-03 13:59:52 +0200

    ETL pipeline MongoDb

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,14 +1,29 @@
+# from pymongo.mongo_client import MongoClient
+# import os
+
+# # Load environment variables from .env file
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# uri = os.getenv("MONGO_DB_URL")
+
+# # Create a new client and connect to the server
+# client = MongoClient(uri)
+
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
+
 
 from pymongo.mongo_client import MongoClient
-import os
-# Load environment variables from .env file
-from dotenv import load_dotenv
-load_dotenv()
-
-uri = os.getenv("MONGO_DB_URL")
+import certifi
+uri = "mongodb+srv://mohammedrizwan0909:Admin123@cluster0.l0h4ner.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri)
+client = MongoClient(uri, tls=True, tlsCAFile=certifi.where())
 
 # Send a ping to confirm a successful connection
 try:
@@ -16,3 +31,16 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
